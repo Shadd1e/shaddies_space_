9104e0d1582481e4fcd1b33a62bb42c78d9fb7e7
Shadd1e <nshadravhn@gmail.com>
2026-03-02 08:43:20 +0100

    price change

--- src/app/automatex/page.js
+++ src/app/automatex/page.js
@@ -30,7 +30,7 @@ export default function AutomateXPage() {
           </p>
 
           <p className="text-2xl font-semibold mb-10">
-            ₦25,000
+            ₦10,000
           </p>
 
           <a
